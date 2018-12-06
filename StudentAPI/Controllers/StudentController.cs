9ff32d0e297a33dfa1be1978e0b80a1e2d287f11
anish25 <anishgandhi25@gmail.com>
2018-12-05 22:55:15 -0500

    commit 2

--- StudentAPI/Controllers/StudentController.cs
+++ StudentAPI/Controllers/StudentController.cs
@@ -19,6 +19,64 @@ namespace StudentAPI.Controllers
             this.db = db;
         }
 
-        
+        [HttpGet("{id}")]
+        public ActionResult Get(int id) {
+
+            Student student = db.Students.Find(id);
+            if (student == null) {
+                return NotFound();
+
+            }
+            return Ok(student);
+        }
+
+        //post: api/albums
+        [HttpPost]
+
+        public ActionResult Post([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Students.Add(student);
+            db.SaveChanges();
+            return CreatedAtAction("Post", student);
+        }
+
+
+        //PUT: apiSgtudents/s
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody]Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            db.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return NoContent();
+        }
+
+
+        //DELETE :api/students
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Student student = db.Students.Find(id);
+            if(student == null)
+            {
+                return NotFound();
+                }
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return ok();
+        }
+
+        private ActionResult ok()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
